9580bf6dab57cf9ac215185e46829193598c0bda
WONJONG-GONG <wonjong.gong@zeroichi.biz>
2023-09-07 18:55:35 +0900

    `getStaticProps()`, `useSWR()` combination

--- pages/App.tsx
+++ pages/App.tsx
@@ -1,56 +1,50 @@
-import { GetStaticProps } from 'next';
 import axios from 'axios';
 import useSWR from 'swr';
-import { useEffect, useState } from 'react';
 import dump from '../dump.json';
+import moment from 'moment-timezone';
+import { useEffect, useState } from 'react';
+import { GetStaticProps } from 'next';
+import { fetchCurrentTaxis } from './api/index';
 
-// export const getStaticProps: GetStaticProps<any> = async () => {
-//     const { getTaxiData } = require('../lib/helpers');
-//     const data = await getTaxiData();
-
-//     return {
-//         props: {
-//             data
-//         },
-//         revalidate: 1
-//     }
-// }
+export const getStaticProps: GetStaticProps<any> = async () => {
+    return {
+        props: {
+            data: await fetchCurrentTaxis()
+        },
+        revalidate: 1
+    }
+}
 
 const fetcher = (url: string) => axios.get(url).then(res => res.data);
 
 const App: React.FC<any> = (props) => {
-    const { data, error } = useSWR<any>('/api', fetcher, {fallbackData: dump, refreshInterval: 100});
-
-    console.log('*** from browser', data.features[0].properties.timestamp, data.features[0].properties.taxi_count);
-
-    const ts = data ? data.features[0].properties.timestamp : '';
-    const count = data ? data.features[0].properties.taxi_count : '';
-    const coordinates = data ? data.features[0].geometry.coordinates : [];
-
     const [cnt, setCnt] = useState<number>(0);
-
-    function refresh() {
-        setCnt(cnt + 1);
-    }
-
+    const { data, error } = useSWR<any>('/api', fetcher, {fallbackData: props.data, refreshInterval: 100});
+    
+    const { ts, count } = data.features[0].properties.timestamp;
+    const coordinates = data.features[0].geometry.coordinates;
+    
     useEffect(() => {
         setCnt(cnt + 1);
     }, [data]);
 
+    console.log('from browser', data.features[0].properties.timestamp, data.features[0].properties.taxi_count);
+    
     return (
         <>
             <div>As of 
-                <p>{new Date(ts).toString()}</p>
-                there are {count} taxis available in Singapore
+                <p>{moment.tz(ts, 'Asia/Singapore').toString()}</p>
+                there are {count} taxis available in Singapore<br/>
+                component re-rendered {cnt} times
             </div>
             <br/>
             {
-                coordinates.map((each: any) => {
+                coordinates.map((each: any, index: number) => {
                     return (
-                        <>
+                        <div key={index}>
                             [ <span>{each[0]}</span>, &emsp;&emsp; <span>{each[1]}</span> ]
                             <br/>
-                        </>
+                        </div>
                     )
                 })
             }
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,7 +1,9 @@
-import { getTaxiData } from '../../lib/helpers'
+export async function fetchCurrentTaxis() {
+    return fetch('https://api.data.gov.sg/v1/transport/taxi-availability').then(res => res.json());
+}
 
 export default async function handler(req, res){
-    const data = await fetch("https://api.data.gov.sg/v1/transport/taxi-availability").then(res => res.json());
+    const data = await fetchCurrentTaxis();
 
     console.log('from handler', data.features[0].properties.timestamp, data.features[0].properties.taxi_count);
 
